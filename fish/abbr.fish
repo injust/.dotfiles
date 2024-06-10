ba96fbad904511c5f133d38d5d1d195ffc3f97c7
Justin Su <injustsu@gmail.com>
2024-06-10 18:27:03 -0400

    Overhaul fish and Git aliases

---
+++ fish/abbr.fish
@@ -0,0 +1,5 @@
+abbr --add ll -- ls -AFhl
+abbr --add wget -- wget2
+
+# Inspired by https://fishshell.com/docs/current/interactive.html#abbreviations
+abbr --add dotdot --regex '^\.{2,}$' --function __multicd
